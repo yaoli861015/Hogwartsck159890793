af9c81f8c8f5633995bca514639e4efc4add1651
yaoli861015 <726476490@QQ.COM>
2020-10-24 22:56:28 +0800

    pytest 数据驱动

---
+++ testing/test_allure.py
@@ -0,0 +1,24 @@
+# -*-coding:utf-8-*-
+__author__ = 'YL'
+
+import allure
+import pytest
+
+
+@allure.feature("登录模块")
+class TestLogin:
+    @allure.story("登录成功")
+    def test_login_success(self):
+        print("登录成功")
+
+    @allure.story("登录失败")
+    def test_login_fail(self):
+        print("登录失败")
+
+    @allure.story("登录用户名缺失")
+    def test_login_lose_user_name(self):
+        print("登录缺失用户名")
+
+    @allure.story("登录密码缺失")
+    def test_login_lose_password(self):
+        print("登录缺失密码")
