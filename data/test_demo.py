bc12a42abe1b36114b64de0dac1bde638ecfb5bf
yaoli861015 <726476490@qq.com>
2022-02-16 22:54:03 +0800

    22.46

--- data/test_demo.py
+++ data/test_demo.py
@@ -1,9 +1,21 @@
 # coding:utf-8
 import time
 
+import pytest
 from selenium import webdriver
 
+test_user_data = ["tom", "jerry"]
 
-def test_steps_demo():
-    driver = webdriver.Chrome()
-    driver.get("https:www.baidu.com")
+
+@pytest.fixture(scope="module")
+def login_r(request):
+    user = request.param
+    print(f"\n登录的用户名：{user}")
+    return user
+
+
+@pytest.mark.parametrize("login_r", test_user_data, indirect=True)
+def test_login(login_r):
+    a = login_r
+    print(f"测试用例中login返回的值是：{a}")
+    assert a != ''
